913c9f6efe28a0c86ac199fc9cc5ae1a65d48d31
aphy358 <358937572@qq.com>
2018-06-05 22:13:51 +0800

    f

--- src/components/HotelSelectPanel/index.vue
+++ src/components/HotelSelectPanel/index.vue
@@ -1,125 +1,122 @@
 <!-- 酒店选择面板 -->
 <template>
-    <section>
+    <section class="kwh-inner">
         <!-- 酒店关键字模板城市部分 -->
         <div v-if="cityList.length > 0">
             <div class="kwh-block-title">城市</div>
             <ul class="kwh-block-content-list city">
-                <% for(var i = 0; i < dataList.length; i++){ var o = dataList[i]; %>
-                <li class="kwh-block-content-item city" data-cityname="<%= o.aname %>" data-citytype="<%= o.type %>" data-cityid="<%= o.cityid %>" data-track="公共_关键字_酒店面板_城市项">     
-                    <%= o.cityStr %>
-                    <span class="area"><%= o.countryName %></span>
-                    <span class="area"><%= o.provinceName %></span>
+                <li v-for="city of cityList" :key="city.cityid" 
+                  class="kwh-block-content-item city" 
+                  :cityname="city.aname" 
+                  :citytype="city.type" 
+                  :cityid="city.cityid" >
+                    <span v-html="city.cityStr"></span>
+                    <span class="area">{{city.countryName}}</span>
+                    <span class="area">{{city.provinceName}}</span>
                 </li>
-                <% } %>
             </ul>
         </div>
 
         <!-- 酒店关键字模板酒店名称部分 -->
-        <div>
+        <div v-if="hotelList.length > 0">
             <div class="kwh-block-title">酒店</div>
             <ul class="kwh-block-content-list hotel">
-                <% for(var i = 0; i < dataList.length; i++){ var o = dataList[i]; %>
-                <li class="kwh-block-content-item hotel">
-                    <a href="<%= o.link %>" target="_blank" data-hotelid="<%= o.id %>">
-                        <span class="kwh-hotel-name"><%= o.hotelStr %></span>
-                        <span class="area"><%= o.countryName %></span>
-                        <span class="area"><%= o.provinceName %></span>
-                        <span class="area"><%= o.cityName %></span>
+                <li v-for="hotel of hotelList" :key="hotel.id"
+                  class="kwh-block-content-item hotel">
+                    <a href="#" target="_blank" :hotelid="hotel.id">
+                        <span class="kwh-hotel-name" v-html="hotel.hotelStr"></span>
+                        <span class="area">{{hotel.countryName}}</span>
+                        <span class="area">{{hotel.provinceName}}</span>
+                        <span class="area">{{hotel.cityName}}</span>
                     </a>
                 </li>
-                <% } %>
             </ul>
+        </div>
+
+        <!-- 酒店关键字模板酒店名称部分 -->
+        <div v-if="cityList.length <= 0 && hotelList.length <= 0">
+            <p class="kwh-no-content" style="color: red; display: block;">无相关城市/酒店</p>
         </div>
     </section>
 </template>
 
 <script>
-
 export default {
-  name: 'HotelSelectPanel',
-  data(){
-    return {
-    }
+  name: "HotelSelectPanel",
+  data() {
+    return {};
   },
   props: {
     hotelList: {},
-    cityList: {},
+    cityList: {}
   },
-  computed: {
-    
-  },
-  components: {
-  }
-}
+  computed: {},
+  components: {}
+};
 </script>
 
 <!-- 覆盖 element 原有样式，注意这里不能用 scoped 限定作用域 -->
 <style lang="scss">
-
+.key {
+  color: orange;
+}
 </style>
 
-<style lang="scss">
-
+<style scoped lang="scss">
 // 城市关键字选择面板
-.key-word-hotel-wrap{
-    position: absolute;
-    font-size: 14px;
-    color: #333333;
-    background: white;
-    box-shadow: 0 1px 5px #ababab;
-    overflow: hidden;
-    z-index: 9999;
+.key-word-hotel-wrap {
+  position: absolute;
+  font-size: 14px;
+  color: #333333;
+  background: white;
+  box-shadow: 0 1px 5px #ababab;
+  overflow: hidden;
+  z-index: 9999;
 
-    @at-root .kwh-inner{
-        min-width: 350px;
-        margin: 20px;
+  @at-root .kwh-inner {
+    min-width: 350px;
+    margin: 20px;
 
-        @at-root .kwh-no-content{
-            color: red;
-            font-size: 16px;
-            text-align: center;
-        }
-
-        @at-root .kwh-block{
-
-            @at-root .kwh-block-title{
-                color: #339afc;
-                font-size: 15px;
-                border-bottom: solid 1px #339afc;
-                font-weight: bold;
-            }
-    
-            @at-root .kwh-block-content-list{
-                font-size: 14px;
-                margin: 10px 0;
-
-                @at-root .kwh-block-content-item{
-                    line-height: 25px;
-                    cursor: context-menu;
-
-                    a{
-                        display: block;
-                        text-decoration: none;
-                        color: #333;
-                    }
-
-                    &:hover{
-                        background: #cce3ff;
-                    }
-
-                    .key{
-                        color: orange;
-                    }
-
-                    .area{
-                        color: #aaa;
-                        float: right;
-                        margin-left: 10px;
-                    }
-                }
-            }
-        }
+    @at-root .kwh-no-content {
+      color: red;
+      font-size: 16px;
+      text-align: center;
     }
+
+    @at-root .kwh-block {
+      @at-root .kwh-block-title {
+        color: #339afc;
+        font-size: 15px;
+        border-bottom: solid 1px #339afc;
+        font-weight: bold;
+      }
+
+      @at-root .kwh-block-content-list {
+        font-size: 14px;
+        margin: 10px 0;
+
+        @at-root .kwh-block-content-item {
+          line-height: 25px;
+          cursor: context-menu;
+
+          a {
+            display: block;
+            text-decoration: none;
+            color: #333;
+          }
+
+          &:hover {
+            background: #cce3ff;
+          }
+
+          .area {
+            color: #aaa;
+            float: right;
+            margin-left: 10px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- src/components/SearchLine/modules/KerywordSuggest.vue
+++ src/components/SearchLine/modules/KerywordSuggest.vue
@@ -1,12 +1,13 @@
 <template>
   <el-popover
-    width="550"
+    :width="keyword === '' ? 550 : 390"
     style="width:260px;display: inline-block;"
     trigger="focus"
     placement="bottom-start"
     v-model="visible2">
 
-    <CitySelectPanel :cityType="cityType" />
+    <CitySelectPanel v-if="showPanel === 1" :cityType="cityType" />
+    <HotelSelectPanel v-if="showPanel === 2" :hotelList="hotelList" :cityList="cityList" />
 
     <el-input
       size="small"
@@ -24,18 +25,20 @@
 
 <script>
 import CitySelectPanel from '../../CitySelectPanel'
+import HotelSelectPanel from '../../HotelSelectPanel'
 
 export default {
   name: '',
   data() {
     return {
+      keyword: '',
       visible2: false,
       loading: false,
 
       // 1：显示默认的城市选择面板、2：显示查询到的酒店列表面板
       showPanel: 1,
 
-      cityList: [{"countryName":"Z-中国","count":534,"provinceName":"G广东","type":0,"aname":"深圳","cityid":70002}],
+      cityList: [],
 
       hotelList: [],
 
@@ -51,10 +54,13 @@ export default {
     
   },
   components: {
-    CitySelectPanel
+    CitySelectPanel,
+    HotelSelectPanel
   },
   methods: {
     remoteMethod(keyword) {
+      this.keyword = keyword
+
       if (keyword !== '') {
         this.showPanel = 2
 
@@ -76,7 +82,7 @@ export default {
       let res_city = await this.$api.hotelList.syncGetCity(param)
 
       if(res_city.returnCode === 1 && res_city.dataList){
-        this.cityList = res_city.dataList
+        this.cityList = this.setHighlightStr(res_city.dataList, param.keys, 'aname', 'cityStr')
       }
     },
 
@@ -85,8 +91,37 @@ export default {
       let res_hotel = await this.$api.hotelList.syncGetHotels(param)
 
       if(res_hotel.returnCode === 1 && res_hotel.dataList){
-        this.hotelList = res_hotel.dataList
+        this.hotelList = this.setHighlightStr(res_hotel.dataList, param.keys, 'name', 'hotelStr')
       }
+    },
+
+    /**
+     * 将关键字搜索返回的结果设置关键字高亮
+     * dataList：待处理数组
+     * keyword：关键字，如 '深圳'、'上海'
+     * name：数组中高亮处理的原始字段名，如 'aname'、'name'
+     * strName：高亮处理后新添加的字段名，如 'cityStr'、'hotelStr'
+     */
+    setHighlightStr(dataList, keyword, name, strName){
+      dataList.forEach(n => {
+        n.cityName = n.cityName ? this.trim(n.cityName) : ''
+        n.provinceName = this.trim(n.provinceName)
+        n.countryName = this.trim(n.countryName)
+        n[strName] = ''
+
+        for (let i = 0; i < n[name].length; i++) {
+          (~keyword.indexOf(n[name][i]))
+            ? n[strName] += '<span class="key">' + n[name][i] + '</span>'
+            : n[strName] += n[name][i]
+        }
+      })
+
+      return dataList
+    },
+
+    // 去除字符串中所有的大小字母和 '-'
+    trim(str){
+      return str.replace(/[A-Za-z-]/g, '')
     },
   }
 }
