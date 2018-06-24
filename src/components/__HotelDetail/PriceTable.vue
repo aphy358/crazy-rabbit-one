ba1ad416446ffc5f7b1f49edad9aab9a264a3d16
Joker <chenhua385148231@126.com>
2018-06-24 11:08:37 +0800

    f

--- src/components/__HotelDetail/PriceTable.vue
+++ src/components/__HotelDetail/PriceTable.vue
@@ -3,17 +3,22 @@
 <template>
   <div class="price-table-outer">
     <div class="price-table-inner">
+
       <!-- 进度条 -->
-      <div class="progress-outer" v-if="hotel && hotel.percentage > 0" style="position: relative;margin-bottom: 10px;">
-        <el-progress :text-inside="true" :stroke-width="18" :show-text=false
-          :percentage="hotel.percentage" 
-          :color="hotel.color"
-          ></el-progress>
-        <div style="position: absolute;top: 0;width: 100%;text-align: center;color: rgb(21, 169, 94);line-height: 18px;">正在查询最低价，请稍候...</div>
-      </div>
+      <el-collapse :value="hotel && hotel.percentage > 0 ? showv : 0">
+          <el-collapse-item name="70">
+              <div class="progress-outer" style="position: relative;margin-bottom: 10px;">
+                <el-progress :text-inside="true" :stroke-width="18" :show-text=false
+                  :percentage="hotel ? hotel.percentage : 0" 
+                  :color="hotel ? hotel.color : 'rgba(255, 45, 0, 0.7)'"
+                  ></el-progress>
+                <div style="position: absolute;top: 0;width: 100%;text-align: center;color: rgb(21, 169, 94);line-height: 18px;">正在查询最低价，请稍候...</div>
+              </div>
+          </el-collapse-item>
+      </el-collapse>
       
       <div class="fzg-loading-wrap" v-if="!hotel || !hotel.priceList">
-        <img src="https://qnb.oss-cn-shenzhen.aliyuncs.com/real_1514022140288.gif" style="display: block;margin: auto;"/>
+        <img src="https://qnb.oss-cn-shenzhen.aliyuncs.com/real_1514022140288.gif" style="display: block;margin: 20px auto;"/>
       </div>
       
       <PriceList v-if="hotel && hotel.priceList" :priceList="hotel.priceList" />
@@ -29,6 +34,7 @@ export default {
 
   data(){
     return {
+      showv: '70',
     }
   },
 
@@ -52,6 +58,28 @@ export default {
 .price-table-outer{
   padding-bottom: 20px;
   background: white;
+  
+  .el-collapse {
+    position: relative;
+    border-top: none;
+    border-bottom: none;
+  }
+
+  .el-collapse-item:last-child{
+    margin-bottom: 0;
+  }
+
+  .el-collapse-item__wrap{
+    border-bottom: none;
+  }
+
+  .el-collapse-item__header{
+    display: none;
+  }
+
+  .el-collapse-item__content{
+    padding-bottom: 5px;
+  }
   
   @at-root .price-table-inner{
     width: 1200px;
