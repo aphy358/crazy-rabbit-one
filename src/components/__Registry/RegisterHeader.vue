9d877c024594bd2a4ae204f54405e15a5f6fbdde
aphy358 <358937572@qq.com>
2018-07-04 22:42:08 +0800

    f

---
+++ src/components/__Registry/RegisterHeader.vue
@@ -0,0 +1,68 @@
+
+<!-- 注册页面头部说明 -->
+<template>
+  <div>
+    <div class="register-head clearfix">
+        <h5 class="register-title fl">注册账号</h5>
+        <div class="register-h-r fr">
+            <p class="fl">已有房掌柜账号？</p>
+            <router-link class="to-login fl" to="/registry">直接登录</router-link>
+        </div>
+    </div>
+
+    <div class="tips">
+        <i class="icon"></i>
+        <span class="tips-title">温馨提示：</span>
+        如您已在我司有合作，请先咨询注册组0755-33336999索要贵公司账号，无需重复注册，谢谢合作！
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterHeader',
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/jl_sprites.scss";
+
+.register-head{
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px solid #E3E3E3;
+  
+  .register-title{
+    font-size: 24px;
+    color: #666666;
+    margin-left: 30px;
+  }
+  
+  .register-h-r{
+    font-size: 16px;
+    margin-right: 30px;
+    
+    .to-login{
+      color: #3399ff;
+      text-decoration: underline;
+    }
+  }
+}
+
+.tips{
+  margin-left: 30px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 14px;
+  .icon{
+    display: inline-block;
+    vertical-align: middle;
+    @include jl_sprites;
+    @include trumpet;
+  }
+  .tips-title{
+    color: #ffa825;
+    margin-left: 10px;
+  }
+}
+</style>
--- src/views/registry.vue
+++ src/views/registry.vue
@@ -3,20 +3,8 @@
 <template>
   <div class="registry-main-box">
       <div v-if="!registSuccess" class="main">
-          <div class="register-head clearfix">
-              <h5 class="register-title fl">注册账号</h5>
-              <div class="register-h-r fr">
-                  <p class="fl">已有房掌柜账号？</p>
-                  <router-link class="to-login fl" to="/registry">直接登录</router-link>
-              </div>
-          </div>
-
-          <div class="tips">
-              <i class="icon"></i>
-              <span class="tips-title">温馨提示：</span>
-              如您已在我司有合作，请先咨询注册组0755-33336999索要贵公司账号，无需重复注册，谢谢合作！
-          </div>
-
+          <!-- 头部 -->
+          <RegisterHeader />
 
           <div class="company-message">
               <h6 class="company-title">企业基本信息</h6>
@@ -247,6 +235,7 @@
 import { area } from "../assets/area.js";
 import { validator } from "../components/validator.js";
 import { Message } from "element-ui"
+import RegisterHeader from '../components/__Registry/RegisterHeader'
 import RegistSuccess from '../components/__Registry/RegistSuccess'
 
 const { countrys, states, citys } = area
@@ -369,6 +358,7 @@ export default {
   },
 
   components: {
+    RegisterHeader,
     RegistSuccess,
   },
 
@@ -725,45 +715,6 @@ export default {
     background-color: #fff;
     box-shadow: 0 0 10px #eeeeee;
     overflow: hidden;
-    
-    @at-root .register-head{
-      height: 60px;
-      line-height: 60px;
-      border-bottom: 1px solid #E3E3E3;
-      
-      .register-title{
-        font-size: 24px;
-        color: #666666;
-        margin-left: 30px;
-      }
-      
-      .register-h-r{
-        font-size: 16px;
-        margin-right: 30px;
-        
-        .to-login{
-          color: #3399ff;
-          text-decoration: underline;
-        }
-      }
-    }
-    
-    @at-root .tips{
-      margin-left: 30px;
-      height: 40px;
-      line-height: 40px;
-      font-size: 14px;
-      .icon{
-        display: inline-block;
-        vertical-align: middle;
-        @include jl_sprites;
-        @include trumpet;
-      }
-      .tips-title{
-        color: #ffa825;
-        margin-left: 10px;
-      }
-    }
   }
 }
 
