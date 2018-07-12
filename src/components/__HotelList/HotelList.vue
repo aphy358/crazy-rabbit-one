257735ddbe89a2d08a9e723827938c6e11b986f0
Joker <chenhua385148231@126.com>
2018-07-12 16:44:46 +0800

    f

--- src/components/__HotelList/HotelList.vue
+++ src/components/__HotelList/HotelList.vue
@@ -95,6 +95,7 @@
 <script>
 import PriceList from "../common/PriceList";
 import Velocity from 'velocity-animate';
+import { openMap } from "../util.js";
 
 export default {
   name: "",
@@ -181,15 +182,7 @@ export default {
     },
 
     popMap(hotel){
-      sessionStorage.setItem('hotelMap', JSON.stringify(hotel))
-
-      let iTop = (window.screen.availHeight - 30 - 500) / 2
-      let iLeft = (window.screen.availWidth - 10 - 650) / 2
-      let _link = hotel.countryType == 0
-        ? '/baiduMap.html'
-        : '/googleMap.html'
-
-      window.open(_link, hotel.infoName, 'height=500,innerHeight=500,width=650,innerWidth=650,top=' + iTop + ',left=' + iLeft + ',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
+      openMap(hotel)
     }
   },
 
--- src/components/util.js
+++ src/components/util.js
@@ -79,3 +79,15 @@ export const remoteJS = (srcs) => {
 }
 
 
+// 打开地图
+export const openMap = (hotel) => {
+  sessionStorage.setItem('hotelMap', JSON.stringify(hotel))
+
+  let iTop = (window.screen.availHeight - 30 - 500) / 2
+  let iLeft = (window.screen.availWidth - 10 - 650) / 2
+  let _link = hotel.countryType == 0
+    ? '/baiduMap.html'
+    : '/googleMap.html'
+
+  window.open(_link, hotel.infoName, 'height=500,innerHeight=500,width=650,innerWidth=650,top=' + iTop + ',left=' + iLeft + ',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
+}
