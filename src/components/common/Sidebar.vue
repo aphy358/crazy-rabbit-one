fe48324b39927a7f96ab681c59bb1d6238ec6324
Tsingwu <1211751992@qq.com>
2018-07-18 10:55:32 +0800

    侧边栏

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -1,6 +1,15 @@
 <!-- 组件说明 -->
 <template>
-
+	<div class="sidebar">
+		<div class="sidebar-btn">
+			<el-button type="warning" icon="el-icon-star-off" circle></el-button>
+			<el-button type="success" icon="el-icon-arrow-up" circle></el-button>
+		</div>
+		
+		<div class="sidebar-spread">
+		
+		</div>
+	</div>
 </template>
 
 <script>
