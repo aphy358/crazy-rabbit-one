e9b260de6f7c4db663789f38b5201f908d95b1b7
Joker <chenhua385148231@126.com>
2018-07-24 10:34:40 +0800

    f

--- tests/unit/layout/header.spec.js
+++ tests/unit/layout/header.spec.js
@@ -1,7 +1,9 @@
 import { expect } from 'chai'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+
 import Vuex from 'vuex'
 import home from '@/store/modules/home'
+
 import Header from '@/layout/header.vue'
 
 import { addDays } from "@/util.js"
@@ -14,6 +16,10 @@ Vue.use(api)
 const localVue = createLocalVue()
 
 localVue.use(Vuex)
+
+
+// Date.prototype.Format=function(fmt){var o={"M+":this.getMonth()+1,"d+":this.getDate(),"h+":this.getHours(),"m+":this.getMinutes(),"s+":this.getSeconds(),"q+":Math.floor((this.getMonth()+3)/3),"S":this.getMilliseconds()};if(/(y+)/.test(fmt)){fmt=fmt.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length))}for(var k in o){if(new RegExp("("+k+")").test(fmt)){fmt=fmt.replace(RegExp.$1,(RegExp.$1.length==1)?(o[k]):(("00"+o[k]).substr((""+o[k]).length)))}}return fmt};
+
 
 describe('layout 目录下的 header.vue', () => {
 
@@ -39,14 +45,25 @@ describe('layout 目录下的 header.vue', () => {
       state: state,
       modules: {
         home
+      },
+      mutations: {
+        // 设置状态的公共函数
+        setCommonState(state, payload){
+          _setCommonState(state, payload)
+        },
       }
     })
 
   })
 
-  it('方法', () => {
+  it('方法', (done) => {
     const wrapper = shallowMount(Header, { store, localVue })
     // wrapper.find('.i-t-n-user-wrap').trigger('click')
-    
+
+    // wrapper.vm.$nextTick(() => {
+    //   done()
+    // })
+
+    done()
   })
 })
