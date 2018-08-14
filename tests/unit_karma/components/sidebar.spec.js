6bda7a5ac677d8853daba308727bf5ac03f8ff99
Tsingwu <1211751992@qq.com>
2018-08-14 15:31:20 +0800

    解决侧边栏测试用例在登录未完成的情况下就开始测试导致的报错问题

--- tests/unit_karma/components/sidebar.spec.js
+++ tests/unit_karma/components/sidebar.spec.js
@@ -15,7 +15,7 @@ describe('公共组件-侧边栏', () => {
     localVue
   });
   
-  before(() => {
+  before((done) => {
     api.common.syncCheckcode().then(res => {
       let params = {
         accountCode: 'sz2747',
@@ -25,58 +25,52 @@ describe('公共组件-侧边栏', () => {
         rememberMe: false
       };
       
-      api.common.syncLogin(params).then(res => { })
+      api.common.syncLogin(params).then(res => {
+      })
     });
+    
+    done()
   });
   
   
-  it('加载关注版面', () => {
+  it('加载关注版面', (done) => {
     expect(wrapper.vm.isShowConcern).to.equal(false);
-    
-    setTimeout(() => {
-      wrapper.find('.gz').trigger('click');
-      wrapper.vm.$nextTick(() => {
-        expect(wrapper.vm.isShowConcern).to.equal(true);
-      })
-      // done()
-    }, 1500)
+  
+    wrapper.find('.gz').trigger('click');
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.isShowConcern).to.equal(true);
+      done()
+    })
     
   });
   
-  it('收起关注版面', () => {
-    
-    setTimeout(() => {
-      wrapper.find('.gz').trigger('click');
-
-      expect(wrapper.vm.isShowConcern).to.equal(true);
-      //收起关注版面
-      wrapper.find('.sidebar-gz-icon-hide').trigger('click');
-      
-      wrapper.vm.$nextTick(() => {
-        expect(wrapper.vm.isShowConcern).to.equal(false);
-      })
-
-      // done()
-    }, 1500)
+  it('收起关注版面', (done) => {
+    wrapper.find('.gz').trigger('click');
+  
+    expect(wrapper.vm.isShowConcern).to.equal(true);
+    //收起关注版面
+    wrapper.find('.sidebar-gz-icon-hide').trigger('click');
+  
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.isShowConcern).to.equal(false);
+      done()
+    })
     
   });
   
-  it('点击遮罩层收起关注版面', () => {
-    
-    setTimeout(() => {
-      wrapper.find('.gz').trigger('click');
-
-      expect(wrapper.vm.isShowConcern).to.equal(true);
-      //收起关注版面
-      wrapper.find('.sidebar-gz-mask').trigger('click');
-      
-      wrapper.vm.$nextTick(() => {
-        expect(wrapper.vm.isShowConcern).to.equal(false);
-      })
-
-      // done()
-
-    }, 1500)
+  it('点击遮罩层收起关注版面', (done) => {
+  
+    wrapper.find('.gz').trigger('click');
+  
+    expect(wrapper.vm.isShowConcern).to.equal(true);
+    //收起关注版面
+    wrapper.find('.sidebar-gz-mask').trigger('click');
+  
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.isShowConcern).to.equal(false);
+      done()
+    })
+  
   });
   
   // it('回到顶部', () => {
