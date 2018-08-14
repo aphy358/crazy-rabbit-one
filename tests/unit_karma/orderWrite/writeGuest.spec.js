5bb1f23e85e398b3731db87583601bfb162524d0
Tsingwu <1211751992@qq.com>
2018-08-14 09:54:14 +0800

    订单填写页入住信息测试（部分）

--- tests/unit_karma/orderWrite/writeGuest.spec.js
+++ tests/unit_karma/orderWrite/writeGuest.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import Router from 'vue-router'
 import { mount, createLocalVue } from '@vue/test-utils'
 import ElementUI from 'element-ui'
-import store from '@/store'
+// import store from '@/store'
 import Vuex from 'vuex'
 
 import WriteGuest from '@/views/orderWrite/writeGuest.vue'
@@ -16,14 +16,32 @@ localVue.use(Vuex);
 const router = new Router();
 
 describe('订单填写页>入住信息', () => {
-  // const wrapper = mount(WriteGuest, {
-  //   localVue,
-  //   router,
-  //   store
-  // });
-  //
+  let wrapper = null;
+  
+  before(() => {
+    let store = new Vuex.Store({
+      state: {
+        orderWrite : {
+          roomNum : 2,
+          hotelPrice : {
+            maxPersonNum : 2
+          }
+        }
+      }
+    });
+  
+    wrapper = mount(WriteGuest, {
+      store,
+      localVue,
+      router,
+    });
+    
+  })
+  
   // it('同一入住人填入一项，其他项也必填', () => {
   //   wrapper.vm.modelArr['surname0'] = 'e';
+  //   wrapper.find('.guest-info-box input').trigger('input');
   //   expect(wrapper.vm.error['surname0Msg']).to.equal('不能为空');
   // })
+  
 })
